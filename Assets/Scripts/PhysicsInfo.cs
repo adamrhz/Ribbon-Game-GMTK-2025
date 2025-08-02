773eda640c7b7f229376f63119b11c16b0ae3a8f
Adam Rhazi <78358394+adamrhz@users.noreply.github.com>
2025-08-01 23:25:50 -0400

    Update PhysicsInfo.cs

--- Assets/Scripts/PhysicsInfo.cs
+++ Assets/Scripts/PhysicsInfo.cs
@@ -8,20 +8,20 @@ namespace Ribbon
     public class PhysicsInfo : ScriptableObject
     {
         [Header("Player Physics")]
-        public float Acceleration = 10f;          
-        public float Deceleration = 40f;   
-        public float Friction = 12.5f;      
-        public float MaxSpeed = 40f;
+        public float Acceleration = 20f;          
+        public float Deceleration = 100f;   
+        public float Friction = 40f;      
+        public float MaxSpeed = 9.7f;
 
-        public float AirAcceleration = 10f;
-        public float AirDeceleration = 40f;
-        public float AirDrag = 40f;
+        public float AirAcceleration = 15f;
+        public float AirDeceleration = 80f;
+        public float AirDrag = 10f;
         public int JumpAmount = 2;
-        public float Gravity = 30f;       
-        public float JumpStrength = 15f;  
-        public float DoubleJumpMultiplier = .75f;
-        public float JumpCutoff = 8f;  
-        public float MaxFallSpeed = 30f;  
+        public float Gravity = 20f;       
+        public float JumpStrength = 12.5f;  
+        public float DoubleJumpMultiplier = 0.6f;
+        public float JumpCutoff = 4f;  
+        public float MaxFallSpeed = 20f;  
         public float CoyoteTime = 0.15f;
 
 
