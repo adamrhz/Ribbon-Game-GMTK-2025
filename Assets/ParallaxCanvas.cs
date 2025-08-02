433bb531a4b62bbb0a73aba7ddc2cf9df19fdf70
Adam Rhazi <78358394+adamrhz@users.noreply.github.com>
2025-08-02 09:32:51 -0400

    Parallax Canvas

---
+++ Assets/ParallaxCanvas.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Ribbon
+{
+
+
+
+    public class ParallaxCanvas : MonoBehaviour
+    {
+        public RawImage[] parallaxImages;
+        public float parallaxFactor = 0.1f;
+        public float YClamp = 10;
+        public float YMinClamp = -10;
+
+        public void LateUpdate()
+        {
+            int index = 0;
+            foreach(RawImage parallaxImage in parallaxImages)
+            {
+                Vector2 offset = new Vector2(transform.position.x, Mathf.Clamp(transform.position.y, YMinClamp, YClamp)) * parallaxFactor;
+                parallaxImage.uvRect = new Rect(offset, parallaxImage.uvRect.size);
+                index++;
+            }
+        }
+
+
+    }
+
+}
