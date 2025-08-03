d1a77c77fb29a94e51795ab43559edfad4d7dccf
SharlShark <supergrayshark@gmail.com>
2025-08-03 03:26:31 -0500

    Fixes to the level shit and added Parallax Background

--- Assets/Scripts/UI/ParallaxCanvas.cs
+++ Assets/Scripts/UI/ParallaxCanvas.cs
@@ -11,16 +11,18 @@ namespace Ribbon
     public class ParallaxCanvas : MonoBehaviour
     {
         public RawImage[] parallaxImages;
-        public float parallaxFactor = 0.1f;
+        //public float parallaxFactor = 0.1f;
+        public float[] parallaxFactor;
         public float YClamp = 10;
         public float YMinClamp = -10;
+        
 
         public void LateUpdate()
         {
             int index = 0;
             foreach(RawImage parallaxImage in parallaxImages)
             {
-                Vector2 offset = new Vector2(transform.position.x, Mathf.Clamp(transform.position.y, YMinClamp, YClamp)) * parallaxFactor;
+                Vector2 offset = new Vector2(transform.position.x, Mathf.Clamp(transform.position.y, YMinClamp, YClamp)) * parallaxFactor[index];
                 parallaxImage.uvRect = new Rect(offset, parallaxImage.uvRect.size);
                 index++;
             }
